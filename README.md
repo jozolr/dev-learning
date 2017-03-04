84dc0792d13f9af536ac51c7cb84e633168485cf
Joseph Robert <Joseph Robert>
2017-03-04 08:18:11 -0800

    Fixes

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # HOW TO SETUP THE ENVIRONMENT
+
 ## Installations
 
-#### 1. mocha to run tests
+### 1\. mocha to run tests
 
 ' npm install mocha --serve-dev '
 
-
 ## Let's run it
-#### 1. lunch node server
+
+### 1\. lunch node server
 
 ' npm start '
 
-#### 2. run tests
+### 2\. run tests
 
 ' npm test'
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express  = require('express');
+var express = require('express');
 var app = express();
 var path = require("path");
 var mysql = require('mysql');
@@ -7,25 +7,25 @@ app.use("/stylesheets", express.static(__dirname + '/stylesheets'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
 
 var connection = mysql.createConnection({
-  'host'     : 'sylphidus.c4wigczk921v.us-west-2.rds.amazonaws.com',
-  'user'     : 'jozolr',
-  'password' : 'Password1',
-  'database' : 'sylphidus_bd',
-  'port' : '3306',
-  'acquireTimeout' : 1000000,
-  'connectionLimit' : 10
+    'host': 'sylphidus.c4wigczk921v.us-west-2.rds.amazonaws.com',
+    'user': 'jozolr',
+    'password': 'Password1',
+    'database': 'sylphidus_bd',
+    'port': '3306',
+    'acquireTimeout': 1000000,
+    'connectionLimit': 10
 });
 
-connection.connect(function(err){
-if(!err) {
-    console.log("Database is connected");
-} else {
-    console.log(err);
-}
+connection.connect(function(err) {
+    if (!err) {
+        console.log("Database is connected");
+    } else {
+        console.log(err);
+    }
 });
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/views/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 var server = app.listen(3000);
